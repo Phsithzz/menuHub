b50bdc7ed1f6bfb9e9d75f17b377cb06ab6fdab0
Phsithzz <phoosith2548@gmail.com>
2025-12-16 22:03:46 +0700

    AuthScreen

--- app/src/main/java/com/example/food/screens/HomeScreen.kt
+++ app/src/main/java/com/example/food/screens/HomeScreen.kt
@@ -1,10 +1,38 @@
 package com.example.food.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
-fun HomeScreen(navController: NavController){
-    Text(text="Home Screen")
+fun HomeScreen(navController: NavController) {
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(40.dp),
+            onClick = {
+                navController.navigate("auth") {
+                    popUpTo(navController.graph.startDestinationId) {
+                        inclusive = true
+                    }
+                }
+            },
+            shape = RoundedCornerShape(10.dp)
+        ) {
+            Text(text = "Back")
+        }
+    }
+
 }
+
